1017288faf6353290731acb0e65e6b3388e9b06e
Lucy Qi <l8qi@uwaterloo.ca>
2023-11-25 17:52:46 -0500

    adding css

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Footer, Blog, Possibility, Features, What, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
 
+import './App.css';
+
 
 const App = () => (
   <div className="App">
