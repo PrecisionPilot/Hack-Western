32aae10cd0be7b50a6ee6286aaed892ac6ad5c0e
Sean Wang <seanwaterloo2997@gmail.com>
2023-11-24 23:14:32 -0500

    Ftr: Voice recorder all-in-one

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,7 +1,37 @@
+import pyaudio
+import wave
+
+# -------------------- Speech to file (voice recorder) --------------------
+
+audio = pyaudio.PyAudio()
+stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+frames = []
+
+try:
+    while True:
+        data = stream.read(1024)
+        frames.append(data)
+except KeyboardInterrupt:
+    pass
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+with wave.open("speech.wav", "wb") as sound_file:
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    sound_file.setframerate(44100)
+    sound_file.writeframes(b''.join(frames))
+
+
+
+# -------------------- File to speech recognition --------------------
 from google.cloud import speech
 
+
 client = speech.SpeechClient.from_service_account_file("auth.json")
-file_name = "speech1.mp3"
+file_name = "speech.wav"
 with open(file_name, "rb") as f:
     mp3_data = f.read()
 
@@ -9,8 +39,9 @@ audio_file = speech.RecognitionAudio(content=mp3_data)
 config = speech.RecognitionConfig(
         sample_rate_hertz=44100,
         enable_automatic_punctuation=False,
-        language_code="zh",
+        language_code="en-US",
     )
 response = client.recognize(config=config, audio=audio_file)
 
-print(response.results[0].alternatives[0].transcript)
+# print(response.results[0].alternatives[0].transcript)
+print(response)
