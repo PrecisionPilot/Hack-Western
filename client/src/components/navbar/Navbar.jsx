940aa29c5afb9a9894583362054e59dfac97cc7f
Lucy Qi <l8qi@uwaterloo.ca>
2023-11-25 18:15:24 -0500

    trying these assets

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Navbar
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
