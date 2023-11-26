f3a47687893d8c1e0ff7c689ca6f84920527211e
Sean Wang <seanwaterloo2997@gmail.com>
2023-11-25 20:00:15 -0500

    Eye tracking

--- computer-vision/eyetracker.py
+++ computer-vision/eyetracker.py
@@ -1,48 +1,85 @@
 import cv2
-import pyautogui
+import dlib
+import numpy as np
 
-# Initialize the webcam
+def shape_to_np(shape, dtype="int"):
+    # initialize the list of (x, y)-coordinates
+    coords = np.zeros((68, 2), dtype=dtype)
+    # loop over the 68 facial landmarks and convert them
+    # to a 2-tuple of (x, y)-coordinates
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+    # return the list of (x, y)-coordinates
+    return coords
+
+def eye_on_mask(mask, side):
+    points = [shape[i] for i in side]
+    points = np.array(points, dtype=np.int32)
+    mask = cv2.fillConvexPoly(mask, points, 255)
+    return mask
+
+def contouring(thresh, mid, img, right=False):
+    cnts, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    try:
+        cnt = max(cnts, key = cv2.contourArea)
+        M = cv2.moments(cnt)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        if right:
+            cx += mid
+        cv2.circle(img, (cx, cy), 4, (0, 0, 255), 2)
+    except:
+        pass
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+left = [36, 37, 38, 39, 40, 41]
+right = [42, 43, 44, 45, 46, 47]
+
 cap = cv2.VideoCapture(0)
+ret, img = cap.read()
+thresh = img.copy()
 
-# Load the pre-trained face and eye cascade classifiers
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+cv2.namedWindow('image')
+kernel = np.ones((9, 9), np.uint8)
 
-while True:
-    # Read a frame from the webcam
-    ret, frame = cap.read()
+def nothing(x):
+    pass
+cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
 
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+while(True):
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 1)
+    for rect in rects:
 
-    # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-
-    for (x, y, w, h) in faces:
-        # Draw a rectangle around the face
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-        # Extract the region of interest (ROI) for eyes
-        roi_gray = gray[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-
-        for (ex, ey, ew, eh) in eyes:
-            # Draw rectangles around the eyes
-            cv2.rectangle(frame, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (0, 255, 0), 2)
-
-            # Calculate the midpoint of the eyes
-            eye_midpoint_x = x + ex + ew // 2
-            eye_midpoint_y = y + ey + eh // 2
-
-            # Move the mouse based on eye gaze
-            
-            screen_width, screen_height = pyautogui.size()
-            destination_x = int((eye_midpoint_x / frame.shape[1]) * screen_width)
-            destination_y = int((eye_midpoint_y / frame.shape[0]) * screen_height)
-
-            pyautogui.FAILSAFE = False
-            pyautogui.moveTo(destination_x, destination_y, duration=0.25)
-
-    # Display the frame
-    cv2.imshow("Eye Tracking", frame)
-
+        shape = predictor(gray, rect)
+        shape = shape_to_np(shape)
+        mask = np.zeros(img.shape[:2], dtype=np.uint8)
+        mask = eye_on_mask(mask, left)
+        mask = eye_on_mask(mask, right)
+        mask = cv2.dilate(mask, kernel, 5)
+        eyes = cv2.bitwise_and(img, img, mask=mask)
+        mask = (eyes == [0, 0, 0]).all(axis=2)
+        eyes[mask] = [255, 255, 255]
+        mid = (shape[42][0] + shape[39][0]) // 2
+        eyes_gray = cv2.cvtColor(eyes, cv2.COLOR_BGR2GRAY)
+        threshold = cv2.getTrackbarPos('threshold', 'image')
+        _, thresh = cv2.threshold(eyes_gray, threshold, 255, cv2.THRESH_BINARY)
+        thresh = cv2.erode(thresh, None, iterations=2) #1
+        thresh = cv2.dilate(thresh, None, iterations=4) #2
+        thresh = cv2.medianBlur(thresh, 3) #3
+        thresh = cv2.bitwise_not(thresh)
+        contouring(thresh[:, 0:mid], mid, img)
+        contouring(thresh[:, mid:], mid, img, True)
+        # for (x, y) in shape[36:48]:
+        #     cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
+    # show the image with the face detections + facial landmarks
+    cv2.imshow('eyes', img)
+    cv2.imshow("image", thresh)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
